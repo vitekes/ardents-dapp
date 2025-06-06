595210b410b01d6bfb0667f7dbb03122b98aa758
Viktor Estrada <vitekes@gmail.com>
2025-06-06 23:00:25 +0300

    Update Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-    content: ['./app/**/*.{ts,tsx,mdx}', './components/**/*.{ts,tsx}'],
+    content: ['./src/app/**/*.{ts,tsx,mdx}', './src/components/**/*.{ts,tsx}'],
     theme: { extend: {} },
     plugins: [],
 };
